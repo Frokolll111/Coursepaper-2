ba59ffffaad191bf79188b57f9496fca43feb91f
Frokolll111 <buther1433@gmail.com>
2023-07-03 23:03:50 +0700

    Commit

---
+++ src/main/java/pro/sky/Coursepaper2/Coursepaper2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.Coursepaper2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursepaper2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Coursepaper2Application.class, args);
+	}
+
+}
